5aa74872de8e58f4376b87b051fcc16b32e9a3ff
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-06-28 21:37:06 +0300

    feat: update swagger configuration to use the official swagger documentation

---
+++ SmartMeetingManager/Controllers/MeetingsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartMeetingManager.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MeetingsController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetAllMeetings()
+		{
+			string[] MeetingNames = { "Project Kickoff", "Weekly Sync", "Client Presentation" };
+			return Ok(MeetingNames);
+		}
+	}
+}
--- SmartMeetingManager/Program.cs
+++ SmartMeetingManager/Program.cs
@@ -3,23 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseSwaggerUI(o =>
-{
-	o.SwaggerEndpoint("/openapi/v1.json", "SmartMeetingManager API");
-	o.RoutePrefix = string.Empty; // serve Swagger UI at root /
-});
+
 app.UseAuthorization();
 
 app.MapControllers();
