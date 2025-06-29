737bcd3a3eb85c3d0aaed169539fd4b4df58ec05
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-06-29 22:34:26 +0300

    feat: Add ConnectionString To DB

---
+++ SmartMeetingManager/Data/SmartMeetingManagerDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartMeetingManager.Models;
+
+namespace SmartMeetingManager.Data
+{
+	public class SmartMeetingManagerDbContext : DbContext
+	{
+		public SmartMeetingManagerDbContext(DbContextOptions<SmartMeetingManagerDbContext> options): base(options)
+		{
+
+			
+		}
+		public DbSet<Users> Users { get; set; }
+		public DbSet<Rooms> Rooms { get; set; }
+		public DbSet<Features> Features { get; set; }
+		public DbSet<RoomFeatures> RoomFeatures { get; set; }
+		public DbSet<Meetings> Meetings { get; set; }
+		public DbSet<MinutesOfMeetings> MinutesOfMeetings { get; set; }
+		public DbSet<MeetingAttendees> MeetingAttendees { get; set; }
+		public DbSet<Agendas> Agendas { get; set; }
+		public DbSet<Attachments> Attachments { get; set; }
+		public DbSet<ActionItems> ActionItems { get; set; }
+		public DbSet<DiscussionPoints> DiscussionPoints { get; set; }
+		public DbSet<Decisions> Decisions { get; set; }
+		public DbSet<Notifications> Notifications { get; set; }
+
+		
+
+
+	}
+}
