86649aefe4a7ccea5124f458a5444c9f60edab21
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-07-22 22:47:37 +0300

    fix: update fields in Agendas Model

--- SmartMeetingManager/Models/Agendas.cs
+++ SmartMeetingManager/Models/Agendas.cs
@@ -3,9 +3,9 @@
 	public class Agendas
 	{
 		public int Id { get; set; }
-		public string Topic { get; set; }
-		public string Description { get; set; }
-		public TimeOnly Duration { get; set; }
+		public string Topic { get; set; } = string.Empty;
+		public string? Description { get; set; }
+		public int Duration { get; set; }
 		public int Order { get; set; }
 
 		// Foreign Keys
